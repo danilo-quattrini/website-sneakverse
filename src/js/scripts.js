c4d96d6ea39c3c2a9212eb8cbab0489e2ab39f28
Pau Femenia <pau.femenia@alu.uclm.es>
2024-12-03 15:03:16 +0000

    Añadido las ventanas en Tamaño proporcionado

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,43 +1,47 @@
 const products = [
-    { name: "Nike Air Max Tuned 1", price: "199,99€", image: "shoe1.webp", catogory: "populars" },
-    { name: "Adidas Campus 00s", price: "120€", image: "shoe2.webp", catogory: "sales" },
-    { name: "Nike Air Force 1 Low", price: "99,99€", image: "shoe3.webp", catogory: "news" },
-    { name: "Adidas Samba OG", price: "90€", image: "shoe4.webp", catogory: "populars" },
-    { name: "Nike Dunk Low", price: "95€", image: "shoe5.webp", catogory: "news" },
-    { name: "Nike Dunk Low", price: "120€", image: "shoe6.webp", catogory: "sales" },
-    { name: "Nike Air Force 1 Low", price: "95€", image: "shoe9.webp", catogory: "sales" },
-    { name: "Nike Air Max Tuned 1", price: "179,99€", image: "shoe7.webp", catogory: "populars" },
-    { name: "New Balance 530", price: "119,99€", image: "shoe8.webp", catogory: "news" },
-    { name: "Nike Air Max Tuned 1 Utility", price: "199,99€", image: "shoe10.webp", catogory: "populars" },
+    { name: "Nike Air Max Tuned 1", price: "199,99€", image: "shoe1.webp", category: "populars", video: "shoevideo1.mp4" },
+    { name: "Adidas Campus 00s", price: "120€", image: "shoe2.webp", category: "sales", video: "shoevideo2.mp4" },
+    { name: "Nike Air Force 1 Low", price: "99,99€", image: "shoe3.webp", category: "news", video: "shoevideo3.mp4" },
+    { name: "Adidas Samba OG", price: "90€", image: "shoe4.webp", category: "populars", video: "shoevideo4.mp4" },
+    { name: "Nike Dunk Low", price: "95€", image: "shoe5.webp", category: "news", video: "shoevideo5.mp4" },
+    { name: "Nike Dunk Low", price: "120€", image: "shoe6.webp", category: "sales", video: "shoevideo6.mp4" },
+    { name: "Nike Air Force 1 Low", price: "95€", image: "shoe9.webp", category: "sales", video: "shoevideo7.mp4" },
+    { name: "Nike Air Max Tuned 1", price: "179,99€", image: "shoe7.webp", category: "populars", video: "shoevideo8.mp4" },
+    { name: "New Balance 530", price: "119,99€", image: "shoe8.webp", category: "news", video: "shoevideo9.mp4" },
+    { name: "Nike Air Max Tuned 1 Utility", price: "199,99€", image: "shoe10.webp", category: "populars", video: "shoevideo10.mp4" },
 ];
 
 function showProducts() {
     const containerPopular = document.getElementById('popular-products');
     const containerSales = document.getElementById('sale-products');
-    const containserNews = document.getElementById('new-products');
+    const containerNews = document.getElementById('new-products');
 
     products.forEach(product => {
         const card = `
-            <div class="products-card">
-                <img src="assets/img//${product.image}" alt="${product.name}" class="products-images">
+            <div class="products-card" data-video="${product.video}">
+                <img src="assets/img/${product.image}" alt="${product.name}" class="products-images">
                 <h3>${product.name}</h3>
                 <p>${product.price}</p>
             </div>
         `;
 
-        if (product.catogory === "populars") {
+        if (product.category === "populars") {
             containerPopular.innerHTML += card;
-        } else if (product.catogory === "sales") {
+        } else if (product.category === "sales") {
             containerSales.innerHTML += card;
-        } else if (product.catogory === "news") {
-            containserNews.innerHTML += card;
+        } else if (product.category === "news") {
+            containerNews.innerHTML += card;
         }
     });
 
-    function openModal() {
+    function openModal(event) {
+        const videoSource = event.currentTarget.getAttribute('data-video');
+        const modalVideo = document.querySelector('.modal-video source');
+        modalVideo.src = `assets/img/${videoSource}`;
         document.getElementById("shoeModal").style.display = "block";
+        document.querySelector('.modal-video').load(); // Cargar el nuevo video
     }
-    
+
     function closeModal() {
         document.getElementById("shoeModal").style.display = "none";
     }
@@ -49,7 +53,6 @@ function showProducts() {
     document.querySelectorAll('.products-card').forEach(card => {
         card.addEventListener('click', openModal);
     });
-    
 }
 
 showProducts();
